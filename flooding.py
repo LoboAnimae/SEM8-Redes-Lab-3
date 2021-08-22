b43e33d121a748a4b850697999c406e8020651c4
ricardoVA999 <40800457+ricardoVA999@users.noreply.github.com>
2021-08-22 17:32:50 -0600

    Read files

--- flooding.py
+++ flooding.py
@@ -1,5 +1,15 @@
 import slixmpp
 import asyncio
+import json
 
-topologia = {}
-identificadores = {}
+topo = open("topo-demo.txt")
+names = open("names-demo.txt")
+
+topologia = json.load(topo)['config']
+identificadores = json.load(names)['config']
+
+for i in topologia:
+    print(i, topologia[i])
+
+for i in identificadores:
+    print(i, identificadores[i])
